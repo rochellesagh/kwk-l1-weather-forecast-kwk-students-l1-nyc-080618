3f830353e5e30e6aba952b2e91714ccb2c0b71e2
rochellesagh <rsrochelli@gmail.com>
2018-08-09 19:52:06 +0000

    Done.

--- weather.rb
+++ weather.rb
@@ -1,20 +1,42 @@
 # Your methods should take in a hash as an argument - here's an example of what the hash will look like - the methods should take this hash as an argument and return the desired output. 
 
-# weather = {
-# 	:city => "New York",
-# 	:description => "Partly Cloudy",
-# 	:temperature_farenheit => 66.3
-# }
+weather = {
+	:city => "New York",
+	:description => "Partly Cloudy",
+	:temperature_farenheit => 66.3
+}
 
 # 1. Complete the method below called location, that accepts a hash called weather as an argument so that it returns the value of city from the weather hash. 
+
 def location(weather)
-	
-end
+#   weather_hash =  {"New York"=> "Partly Cloudy"=> 66.3 } 
+#   weather_hash.each do |city, description, temperature_farenheit|
+#     puts "City #{city}, Description #{description}, Temperature_farenheit #{temperature_farenheit}"
+# end
+# return city 
+  weather[:city]
+end 
+
+puts location(weather)
 
 # 2. Using the location method as a guide, write a  method called "description" so it returns the description from the weather hash. 
 
+def location(weather)
+  # weather_hash =  {"New York"=> "Partly Cloudy"=> 66.3 } 
+  # weather_hash.each do |city, description, temperature_farenheit|
+  #   puts "City #{city}, Description #{description}, Temperature_farenheit #{temperature_farenheit}"
+    
+    weather[:description]
+end
+# return description
+puts location(weather)
 
-# 3. Using the methods above and string interpolation, update the method below so that it returns a string that describes the current weather - for example: "The weather in New York is Partly Cloudy. The temperature is 66.1 degrees Farenheit." HINT: the method should RETURN the string, not print it to the terminal. 
+
+
+
+# 3. Using the methods above and string interpolation, update the method below so that it returns a string that describes the current weather - for example: "The weather in New York is Partly Cloudy. The temperature is 66.3 degrees Farenheit." HINT: the method should RETURN the string, not print it to the terminal. 
+
+puts "The weather in #{weather[:city]} is #{weather[:description]}." " ""The temperature is #{weather[:temperature_farenheit]}."
 
 
 #4. Write a method that takes in the weather hash as an argument. 
