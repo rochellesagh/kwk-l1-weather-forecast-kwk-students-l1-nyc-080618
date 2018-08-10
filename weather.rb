a28350077d1916d2402e9d1575e8d2944fe1cc13
rochellesagh <rsrochelli@gmail.com>
2018-08-10 13:43:43 +0000

    automatically backed up by learn

--- weather.rb
+++ weather.rb
@@ -36,13 +36,25 @@ puts location(weather)
 
 # 3. Using the methods above and string interpolation, update the method below so that it returns a string that describes the current weather - for example: "The weather in New York is Partly Cloudy. The temperature is 66.3 degrees Farenheit." HINT: the method should RETURN the string, not print it to the terminal. 
 
-puts "The weather in #{weather[:city]} is #{weather[:description]}." " ""The temperature is #{weather[:temperature_farenheit]}."
+return "The weather in #{weather[:city]} is #{weather[:description]}." " ""The temperature is #{weather[:temperature_farenheit]}."
+end 
 
 
 #4. Write a method that takes in the weather hash as an argument. 
 # You'll want to use the each method to iterate over the hash and print out
 # `The city is New York.` `The description is Partly Cloudy.` `The temperature is 66.3.`
 
+  def current_weather
+weather = {
+	:city => "New York",
+	:description => "Partly Cloudy",
+	:temperature_farenheit => 66.3
+}
+weather.each do |key,value|
+  puts "the #{key} is #{value}."
+  end 
+end 
+current_weather
 
 # BONUS:  Write a method that takes in the weather hash as an argument and converts the farenheit temperature to celcius. Have the method round your answer to two decimal places. Is there a method we can use to round in Ruby?
 
